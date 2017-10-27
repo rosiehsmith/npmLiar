fab4adfefdbc3854387d20b5dd9e08f530ebe2a7
rosiehighsmith <rosie.highsmith@gmail.com>
2017-10-27 07:49:50 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = function(prompt){
+    var prompt = "yes or no?";
+    if (prompt = "yes"){
+        alert("your answer was no.");
+    } 
+    else if (prompt = "no"){
+        alert("your answer was yes.");
+    }
+    else {
+        alert("try harder.");
+    }
+};
